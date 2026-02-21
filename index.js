c2e7ed2d9c41af5704e072df72121ad8ab3d914b
josuegraterol-Senai <josue.graterol@aluno.senai.br>
2026-02-21 15:27:17 -0300

    mudei pra um sol quando tiver nocturno

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 const toggleButton = document.getElementById("toggle-mode");
 const body = document.body;
+const icon = toggleButton.querySelector("i");
+
+function updateIcon() {
+    if (body.classList.contains("dark-mode")) {
+        icon.classList.remove("fa-moon");
+        icon.classList.add("fa-sun");
+    } else {
+        icon.classList.remove("fa-sun");
+        icon.classList.add("fa-moon");
+    }
+}
+
+// Atualiza o ícone ao carregar a página
+updateIcon();
 
 toggleButton.addEventListener("click", () => {
     body.classList.toggle("dark-mode");
+    updateIcon();
 });
